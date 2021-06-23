007c5ea82c899e72b3dfc04908e790036609e7f3
Gilbert <ssenyonjogilbert@gmail.com>
2021-06-23 21:25:52 +0300

    Implemented and tested ParallelIndividualMultiplier

--- app/src/main/java/concurrency/matrix/ParallelIndividualMultiplier.java
+++ app/src/main/java/concurrency/matrix/ParallelIndividualMultiplier.java
@@ -14,13 +14,24 @@ public class ParallelIndividualMultiplier {
         List<Thread> threads = new ArrayList<>();
         int rows1 = matrix1.length;
         int columns2 = matrix2[0].length;
+        Thread.State status;
+
 
         for(int i = 0; i < rows1; i++){
             for(int j = 0; j < columns2; j++) {
                 var task = new IndividualMultiplierTask(result, matrix1, matrix2, i, j);
                 Thread thread = new Thread(task);
+                status = thread.getState();
+                System.out.println("thread status is " + thread.getState());
                 thread.start();
+                System.out.println("thread status is " + thread.getState());
+                status = thread.getState();
+                System.out.println(status); // termin // run
+                  System.out.printf("Thread at %d %d is %s\n", i, j, thread.getName());
                 threads.add(thread);
+
+                System.out.println("thread status is " + thread.getState());
+                status = thread.getState();
 
                 if(threads.size() % 10 == 0) {
                     waitForThreads(threads);
@@ -34,10 +45,13 @@ public class ParallelIndividualMultiplier {
      * @param threads list of threads to wait for
      */
     private static void waitForThreads(List<Thread> threads) {
-        System.out.printf("We have %1 threads\n", threads.size());
+        // System.out.printf("We have %d threads\n", threads.size());
         for(var thread: threads) {
             try {
+
+                System.out.println(thread.getState());
                 thread.join();
+                System.out.println(thread.getState());
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
--- app/src/test/java/concurrency/matrix/ParallelIndividualMultiplierTest.java
+++ app/src/test/java/concurrency/matrix/ParallelIndividualMultiplierTest.java
@@ -1,11 +1,89 @@
 package concurrency.matrix;
 
 import concurrency.TimingExtension;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import static org.assertj.core.api.Assertions.*;
 
 /**
  * Test the running of multiply in ParallelIndividualMultiplierTask
  */
 @ExtendWith(TimingExtension.class)
 public class ParallelIndividualMultiplierTest {
+
+    double[][] matrix1 = { {1, 2, 3}, {4, 5, 6} }; // 2 X 3
+    double[][] matrix2 = { {5, 10}, {10, 15}, {20, 25} }; // 3 X 2
+    double[][] result = new double[2][2];
+
+    // cell 00
+    double cell00 = matrix1[0][0] * matrix2[0][0] + // 1*5 + 2*10 + 3*20
+            matrix1[0][1] * matrix2[1][0] +
+            matrix1[0][2] * matrix2[2][0];
+
+    // cell 01
+    double cell01 = matrix1[0][0] * matrix2[0][1] + // 1*10 + 2*15 + 3*25
+            matrix1[0][1] * matrix2[1][1] +
+            matrix1[0][2] * matrix2[2][1];
+
+    // cell 10
+    double cell10 = matrix1[1][0] * matrix2[0][0] + // 4*5 + 5*10 + 6*20
+            matrix1[1][1] * matrix2[1][0] +
+            matrix1[1][2] * matrix2[2][0];
+
+    // cell 01
+    double cell11 = matrix1[1][0] * matrix2[0][1] + // 4*10 + 5*15 + 6*25
+            matrix1[1][1] * matrix2[1][1] +
+            matrix1[1][2] * matrix2[2][1];
+
+
+    @BeforeEach
+    void setup() {
+        result = new double[2][2];
+    }
+
+    @DisplayName("parallel multiplier gives correct result")
+    @Test
+    void parallelMultiplierIsCorrect() {
+        ParallelIndividualMultiplier.multiply(matrix1, matrix2, result);
+        assertThat(result[0][0]).isEqualTo(cell00);
+        assertThat(result[0][1]).isEqualTo(cell01);
+        assertThat(result[1][0]).isEqualTo(cell10);
+        assertThat(result[1][1]).isEqualTo(cell11);
+    }
+
+    @DisplayName("parallel multiplier running time test")
+    @Test // Takes appr 2500 ms
+    void parallelMultiplierRunningTime() {
+        matrix1 = MatrixGenerator.generate(100, 200); // 100 x 200
+        matrix2 = MatrixGenerator.generate(200, 500); // 200 x 500
+        result = new double[100][500];
+        ParallelIndividualMultiplier.multiply(matrix1, matrix2, result);
+    }
+
+    @DisplayName("serial multiplier running time test")
+    @Test // Takes appr 50 ms
+    void serialMultiplierRunningTime() {
+        matrix1 = MatrixGenerator.generate(100, 200); // 100 x 200
+        matrix2 = MatrixGenerator.generate(200, 500); // 200 x 500
+        result = new double[100][500];
+        SerialMultiplier.multiply(matrix1, matrix2, result);
+    }
+
+    @DisplayName("compare parallel and serial results")
+    @Test
+    void compareSerialAndParallel() {
+        matrix1 = MatrixGenerator.generate(100, 200); // 100 x 200
+        matrix2 = MatrixGenerator.generate(200, 500); // 200 x 500
+        var serialResult = new double[100][500];
+        var parallelResult = new double[100][500];
+        SerialMultiplier.multiply(matrix1, matrix2, serialResult);
+        ParallelIndividualMultiplier.multiply(matrix1, matrix2, parallelResult);
+        for(int r =0; r < serialResult.length; r++) {
+            for(int c = 0; c < serialResult[0].length; c++) {
+                assertThat(serialResult[r][c]).isEqualTo(parallelResult[r][c]);
+            }
+        }
+    }
 }
